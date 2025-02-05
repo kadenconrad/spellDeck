b81ad3e15576a5330780e16c815a275347383104
kaden thrower <kadenconradt@gmail.com>
2025-02-05 14:52:39 -0500

    whoops forgot to add NOW first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { Home } from './pages/Home';
+// import { SpellLibrary } from './pages/SpellLibrary';
+
+// function App() {
+//    return (
+//        <Router>
+//            <Routes>
+//                <Route path="/" element={<Home />} />
+//                <Route path="/spells" element={<SpellLibrary />} />
+//            </Routes>
+//        </Router>
+//    );
+//}
